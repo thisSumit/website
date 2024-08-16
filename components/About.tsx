c20c1af237551660906962bc83615a5ba6f1199f
InternPro-tech <internpro038@gmail.com>
2024-08-16 22:00:55 +0530

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -12,7 +12,7 @@ const About = () => {
         <div className='flex flex-col justify-center md:px-10 md:w-3/4 w-full'>
         <img src="/logo.svg" className='md:w-[25vh] w-[20vh]' alt="" />
         <Separator/>
-        <h1 className='font-bold text-4xl md:mt-8 mt-5 md:text-6xl tracking-tighter items-center flex'>At InternPro, We specialize at createing Career and increase high chances of Recruting in MNCs.</h1>
+        <h1 className='font-bold text-4xl md:mt-8 mt-5 md:text-6xl tracking-tighter items-center flex'>At InternPro, We specialize at creating Career and increase high chances of Recruting in MNCs.</h1>
         <p className='text-xl text-muted-foreground mt-1 mb-4 md:my-4'>We bridge the gap between academic knowledge and industry experience. Our mission is to empower students and young professionals by offering real-world internships and industry-standard project opportunities that prepare them for the challenges of the professional world.</p>
         </div>
         <div className='md:w-1/4 w-full '>
